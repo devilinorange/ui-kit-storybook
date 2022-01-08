ad511d0efd03ad1ae700ee031d4c0bd551e7f9e3
devilinorange <edward.oyun@gmail.com>
2022-01-08 20:26:26 +0700

    feat: add npm workspaces

---
+++ packages/storybook/.storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../../components/src/**/*.stories.mdx",
+    "../../components/src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "framework": "@storybook/react"
+}
